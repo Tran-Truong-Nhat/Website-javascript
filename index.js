766bfd057c8c83a436500f82fe9a2e87d15e4d8e
trantruongnhat02 <truongnhattran02@gmail.com>
2023-05-30 07:42:45 +0700

    website javascript

---
+++ index.js
@@ -0,0 +1,91 @@
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() =>{
+    search.classList.toggle('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=()=>{
+    navbar.classList.toggle('active');
+}
+
+
+var swiper = new Swiper(".product-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+      },
+
+    },
+  });
+
+  var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev",
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+      },
+
+    },
+  });
+
+  var swiper = new Swiper(".review-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
